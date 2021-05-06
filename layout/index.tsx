abb055bb5b9307f2ec76f627f4f5baf3943dfd3d
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-05-06 14:18:12 +0800

    feat:主应用

---
+++ layout/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+interface layoutFcProp {}
+const LayoutFc: FC<layoutFcProp> =(props: layoutFcProp) => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default LayoutFc;
